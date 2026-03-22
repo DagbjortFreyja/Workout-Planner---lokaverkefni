71f1a7e4106902b00f0ce4f7ed6cb66d23c6fcd0
DagbjortFreyja <dagbjortfreyja@gmail.com>
2026-03-22 15:34:04 +0000

    fix port f. render

--- src/index.tsx
+++ src/index.tsx
@@ -235,7 +235,7 @@ export default app;
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: Number(process.env.PORT) || 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
